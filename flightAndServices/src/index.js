0e79b44d36753311950d7b74a0d7cf12d7ce97fc
kohi9noor <116247355+kohi9noor@users.noreply.github.com>
2023-11-30 16:10:28 +0200

    added seed and const data

--- flightAndServices/src/index.js
+++ flightAndServices/src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const db = require("./models/index");
 const ApiRoutes = require("./routes/index.js");
+const sequelize = require("sequelize");
 const PORT = 8000;
-
+const { City, Airport } = require("./models/index");
+const airport = require("./models/airport");
 const setupAndStartServer = async () => {
   const app = express();
 
@@ -15,6 +17,9 @@ const setupAndStartServer = async () => {
   app.listen(PORT, async () => {
     console.log(`Server has been started at ${PORT}`);
   });
+  if (process.env.SYNC_DB) {
+    db.sequelize.sync({ alter: true });
+  }
 };
 
 setupAndStartServer();
