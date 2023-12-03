db5bdb864d86b2ace884fb3cdd46503010fa63b0
kohi9noor <116247355+kohi9noor@users.noreply.github.com>
2023-12-03 08:42:09 +0200

    added seed file files and row data for testing

--- FlightAndSearchService/src/index.js
+++ FlightAndSearchService/src/index.js
@@ -2,13 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/db.js");
 const ApiRoutes = require("./routes/index.js");
+const { Airport, City } = require("./models/index.js");
 const setupServer = async () => {
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use("/api", ApiRoutes);
-  app.listen(PORT, () => {
-    console.log(`Server has started at ${PORT}`);
+  app.listen(PORT, async () => {
+    console.log(`Server is started now ${PORT}`);
   });
 };
 
