cb6d98cd9b660a96303482b01d3bb3e6b552f2fc
kohi9noor <116247355+kohi9noor@users.noreply.github.com>
2023-12-23 15:10:48 +0200

    added admin panel for users

---
+++ AdminPanel/index.js
@@ -0,0 +1,39 @@
+import AdminJS from "adminjs";
+import express from "express";
+import Plugin from "@adminjs/express";
+import { Adapter, Database, Resource } from "@adminjs/sql";
+
+AdminJS.registerAdapter({
+  Database,
+  Resource,
+});
+
+const start = async () => {
+  const app = express();
+
+  const db = await new Adapter("postgresql", {
+    connectionString: "postgres://postgres:kohi9noor@localhost:5432/AuthDB",
+    database: "AuthDB",
+  }).init();
+
+  const admin = new AdminJS({
+    resources: [
+      {
+        resource: db.table("Users"),
+        options: {},
+      },
+    ],
+  });
+
+  admin.watch();
+
+  const router = Plugin.buildRouter(admin);
+
+  app.use(admin.options.rootPath, router);
+
+  app.listen(8080, () => {
+    console.log("app started");
+  });
+};
+
+start();
